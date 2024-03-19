d9cf9a9b687698201795e7e2539f1dae1c497272
YEG4 <ahmed.t.ghonem@gmail.com>
2024-03-19 12:36:57 +0200

    intial commit

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home')
+]
